6c15c094724d4b4d00775e1fa76c7c2f0a801553
zorzr <32872339+zorzr@users.noreply.github.com>
2018-04-23 15:20:15 +0200

    Catalogue

--- src/restaurant/Catalogue.java
+++ src/restaurant/Catalogue.java
@@ -1,4 +1,32 @@
 package restaurant;
+import java.util.ArrayList;
+import menu.*;
 
 public class Catalogue {
+    private ArrayList<MenuElement> dishesList;
+    private ArrayList<MenuElement> drinksList;
+
+    public Catalogue() {
+        dishesList = new ArrayList<>();
+        drinksList = new ArrayList<>();
+    }
+
+    public ArrayList<MenuElement> getDishes() {
+        return dishesList;
+    }
+    public ArrayList<MenuElement> getDrinks() {
+        return drinksList;
+    }
+
+    public void addDish(MenuElement elem) {
+        if (elem.getType() != DRINK) {
+            dishesList.add(elem);
+        }
+    }
+
+    public void addDrink(MenuElement elem) {
+        if (elem.getType() == DRINK) {
+            drinksList.add(elem);
+        }
+    }
 }
