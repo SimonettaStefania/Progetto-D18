f243f8fd7289b83c1e2e5bea7838e81c2f62c145
beard33 <andrea.guzzon33@protonmail.com>
2018-04-23 15:51:35 +0200

    Add Menu class

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,4 +1,62 @@
 package menu;
 
 public class Menu {
+    package menu;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+    public class Menu {
+
+        private String name;
+        private ArrayList <MenuElement> menuElementsList;
+        private double menuCost;
+        private int nMenuGuests;
+
+        public Menu (String name){
+            this.name = name;
+            this.menuElementsList = new ArrayList<>();
+        }
+
+        public void addElement(MenuElement newElement) {
+            menuElementsList.add(newElement);
+        }
+
+        public void removeElement(MenuElement element) {
+            Iterator<MenuElement> iter = menuElementsList.iterator();
+            while (iter.hasNext()) {
+                MenuElement el = iter.next();
+                if (el.equals(element)) {
+                    iter.remove();
+                }
+            }
+
+        }
+
+        public void setNMenuGuests(int nGuest) {
+            this.nMenuGuests = nGuest;
+        }
+
+        public void updateMenuCost(double cost) {
+            this.menuCost = cost;
+        }
+
+        public double getMenuCost() {
+            return menuCost;
+        }
+
+        public int getnMenuGuests() {
+            return nMenuGuests;
+        }
+
+        public String toString () {
+            StringBuilder sb = new StringBuilder();
+            sb.append(this.name).append("\t").append(this.menuCost).append("\t").append(this.nMenuGuests).append("\t");
+            for (MenuElement el : menuElementsList) {
+                sb.append(el.toString()).append("\t");
+            }
+            return sb.toString();
+        }
+    }
+
 }
