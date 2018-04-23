9c7df9d806093301101a4cd5cb2ed8aaefea4191
zorzr <riccardo.zorzato01@universitadipavia.it>
2018-04-23 16:06:13 +0200

    OCD refactor

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -4,55 +4,52 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Menu {
+    private String name;
+    private ArrayList<MenuElement> menuElementsList;
+    private double menuCost;
+    private int nMenuGuests;
 
-        private String name;
-        private ArrayList <MenuElement> menuElementsList;
-        private double menuCost;
-        private int nMenuGuests;
+    public Menu(String name) {
+        this.name = name;
+        this.menuElementsList = new ArrayList<>();
+    }
 
-        public Menu (String name){
-            this.name = name;
-            this.menuElementsList = new ArrayList<>();
-        }
+    public void addElement(MenuElement newElement) {
+        menuElementsList.add(newElement);
+    }
 
-        public void addElement(MenuElement newElement) {
-            menuElementsList.add(newElement);
-        }
-
-        public void removeElement(MenuElement element) {
-            Iterator<MenuElement> iter = menuElementsList.iterator();
-            while (iter.hasNext()) {
-                MenuElement el = iter.next();
-                if (el.equals(element)) {
-                    iter.remove();
-                }
+    public void removeElement(MenuElement element) {
+        Iterator<MenuElement> iter = menuElementsList.iterator();
+        while (iter.hasNext()) {
+            MenuElement el = iter.next();
+            if (el.equals(element)) {
+                iter.remove();
             }
-
         }
+    }
 
-        public void setNMenuGuests(int nGuest) {
-            this.nMenuGuests = nGuest;
+    public void setNMenuGuests(int nGuest) {
+        this.nMenuGuests = nGuest;
+    }
+
+    public void updateMenuCost(double cost) {
+        this.menuCost = cost;
+    }
+
+    public double getMenuCost() {
+        return menuCost;
+    }
+
+    public int getnMenuGuests() {
+        return nMenuGuests;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.name).append("\t").append(this.menuCost).append("\t").append(this.nMenuGuests).append("\t");
+        for (MenuElement el : menuElementsList) {
+            sb.append(el.toString()).append("\t");
         }
-
-        public void updateMenuCost(double cost) {
-            this.menuCost = cost;
-        }
-
-        public double getMenuCost() {
-            return menuCost;
-        }
-
-        public int getnMenuGuests() {
-            return nMenuGuests;
-        }
-
-        public String toString () {
-            StringBuilder sb = new StringBuilder();
-            sb.append(this.name).append("\t").append(this.menuCost).append("\t").append(this.nMenuGuests).append("\t");
-            for (MenuElement el : menuElementsList) {
-                sb.append(el.toString()).append("\t");
-            }
-            return sb.toString();
-        }
-
+        return sb.toString();
+    }
 }
