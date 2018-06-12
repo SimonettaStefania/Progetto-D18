16874b9e84552b816ce1fcb2864817ce5e550023
Fenicottero <borawoiz@gmail.com>
2018-06-12 16:21:32 +0200

    fix date in TestReservation

--- src/restaurant/Reservation.java
+++ src/restaurant/Reservation.java
@@ -3,6 +3,7 @@ package restaurant;
 import menu.Menu;
 import menu.MenuElement;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
@@ -92,8 +93,9 @@ public class Reservation {
 
     public String toString() {                    // TODO add this method in the UML
         StringBuilder sb = new StringBuilder();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         sb.append("Reservation: ").append(this.reservationCode).append("\tCliente: ").append(this.customerNameSurname).append("\tBudget: ").append(this.budget).append("\t nGuest: ").append(this.nGuests)
-                .append("\t ReservationCost: ").append(this.reservationCost).append(("\t eventDate: ")).append(this.eventDate).append("\n");
+                .append("\t ReservationCost: ").append(this.reservationCost).append(("\t eventDate: ")).append(sdf.format(this.eventDate)).append("\n");
         for (Menu m : createdMenu) {
             sb.append(m.toString()).append("\n");
         }
