524b677b2e2eebe9b04e0bd20864380afcf47d44
chiara <chiara.barbera01@universitdipavia.it>
2018-07-17 22:40:46 +0200

    Comments for code documentation in package menu.

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -2,16 +2,32 @@ package menu;
 
 import java.util.ArrayList;
 
+/**
+ * Class that represents a menu in the restaurant. It is described by :
+ *  - a name
+ *  - the number of guests that will order the menu
+ *  - a list of the dishes offered by the menu ; each dish belongs to the class MenuElement
+ */
 public class Menu {
     private String name;
     private ArrayList<MenuElement> menuElementsList;
     private int nMenuGuests;
 
+    /**
+     * Constructor for an empty menu , initialized only with its name and a number of guests
+     * @param name the name of the menu
+     * @param nMenuGuests the number of guests to whom the menu is addressed
+     */
     public Menu(String name, int nMenuGuests) {
         this.name = name;
         this.menuElementsList = new ArrayList<>();
         this.nMenuGuests = nMenuGuests;
     }
+
+    /**
+     * Method that adds a dish in the menu
+     * @param newElement the element to be added
+     */
 
     public void addElement(MenuElement newElement) {
 
@@ -19,13 +35,27 @@ public class Menu {
         menuElementsList.add(newElement);
     }
 
+    /**
+     * Methods that removes a dish from the menu
+     * @param element the element to be removed
+     */
+
     public void removeElement(MenuElement element) {
        menuElementsList.remove(element);
     }
 
+    /**
+     * Sets the number of guests that will chose this menu
+     * @param nGuest the number of guests for the menu
+     */
     public void setNMenuGuests(int nGuest) {
         this.nMenuGuests = nGuest;
     }
+
+    /**
+     * Calculates and returns the menu's cost by adding the prices of each dish in the menu
+     * @return the menu's final cost
+     */
 
     public double getMenuCost() {
         double menuCost = 0;
@@ -34,18 +64,38 @@ public class Menu {
         return menuCost;
     }
 
+    /**
+     * Returns the number of guests to whom the menu is addressed
+     * @return guests number
+     */
+
     public int getnMenuGuests() {
         return nMenuGuests;
     }
+
+    /**
+     * Returns the name of the menu
+     * @return menu's name
+     */
 
     public String getName() {
         return name;
     }
 
     // Method that sorts elementListMenu TODO add this method in the UML
+
+    /**
+     * Method that sorts the dishes of the menu by name ( ascending order )
+     */
+
     public void sortMenuElements(){
         menuElementsList.sort(MenuElement.typeComparator);
     }
+
+    /**
+     * Method that returns a description of the Menu, in particular its name , price
+     * @return the string description
+     */
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -56,9 +106,19 @@ public class Menu {
         return sb.toString();
     }
 
+    /**
+     * Returns the menu's dishes list
+     * @return the dishes list
+     */
+
     public ArrayList<MenuElement> getMenuElementsList() {
         return menuElementsList;
     }
+
+    /**
+     * Sets the menu's name
+     * @param name the name of the menu
+     */
 
     public void setName(String name) {
         this.name = name;
--- src/menu/MenuElement.java
+++ src/menu/MenuElement.java
@@ -3,6 +3,14 @@ package menu;
 import java.util.ArrayList;
 import java.util.Comparator;
 
+/**
+ * Class that represents a single dish in the menu . Each dish is described by :
+ *  - a name and a code
+ *  - a type ( starter, first course, main course , dessert or drink )
+ *  - a price
+ *  - flags setted to true if the dish is vegan or vegetarian or gluten free
+ *  - a list of allergen and a list of ingredients
+ */
 public class MenuElement  {
     private String name, elementCode;
     private DishType type;
@@ -10,6 +18,17 @@ public class MenuElement  {
     private Flags flags;
     private ArrayList<Allergen> allergenList;
     private ArrayList<String> ingredientsList;
+
+    /**
+     * Constructor with parameters
+     * @param name tbe name of the dish
+     * @param elementCode the code of the dish
+     * @param type the type of dish
+     * @param price the price of the dish
+     * @param veg true if the dish is vegan
+     * @param vgt true if the dish is vegetarian
+     * @param cel true if the dish is gluten free
+     */
 
     public MenuElement(String name, String elementCode, DishType type,
                        double price, boolean veg, boolean vgt, boolean cel) {
@@ -23,45 +42,87 @@ public class MenuElement  {
         ingredientsList = new ArrayList<>();
     }
 
+    /**
+     * @return the name of the dish
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return the code of the dish
+     */
+
     public String getElementCode() {
         return elementCode;
     }
-
+    /**
+     * @return the price of the dish
+     */
     public double getPrice() {
         return price;
     }
-
+    /**
+     * @return the type of the dish
+     */
     public DishType getType() {
         return type;
     }
 
+    /**
+     * Checks whether the dish respects the filters or not
+     * @param veg vegan filter
+     * @param vgt vegetarian filter
+     * @param cel celiac filter ( gluten free dish )
+     * @return true if the filters are respected , false otherwise
+     */
     public boolean respectsFilters(boolean veg, boolean vgt, boolean cel){
         return flags.check(veg, vgt, cel);
     }
+
+    /**
+     * Adds an ingredient to the ingredient's list
+     * @param name the ingredient to be added
+     */
 
     public void addIngredient(String name) {
         if (!ingredientsList.contains(name))
             ingredientsList.add(name);
     }
 
+    /**
+     * Adds an allergen to the allergen's list
+     * @param a the allergen to be added
+     */
     public void addAllergen(Allergen a) {
         if (!allergenList.contains(a))
             allergenList.add(a);
     }
+
+    /**
+     * Removes an allergen to the allergen's list
+     * @param a the allergen to be removed
+     */
 
     public void removeAllergen(Allergen a) {
         if (allergenList.contains(a))
             allergenList.remove(a);
     }
 
+    /**
+     * Checks whether an allergen is contained in the allergen's list
+     * @param a the allergen to be checked
+     * @return true if the allergen is present, false otherwise
+     */
     public boolean containsAllergen(Allergen a) {
         return allergenList.contains(a);
 
     }
+
+    /**
+     * Creates a description of the dish's ingredients
+     * @return string description
+     */
     public String showDetails() {
         String tmp = "";
         for (String element : ingredientsList)
@@ -70,9 +131,18 @@ public class MenuElement  {
 
     }
 
+    /**
+     * Creates a description of the dish ( name, price , type of dish )
+     * @return string description
+     */
     public String toString() {
         return name + "\t" + price + " €" + "\t" + type+"\n";
     }
+
+    /**
+     * Creates a description of the dish's allergens
+     * @return string description
+     */
 
     public String showAllergenes() {
         String tmp = "";
@@ -81,6 +151,11 @@ public class MenuElement  {
         return tmp;
     }
 
+    /**
+     * Comparator for the type of dishes. Its method compare( ) takes two MenuElements e1, e2 as parameters and returns
+     * 0 if e1 and e2 are dishes of the same type
+     */
+
     public static Comparator <MenuElement> typeComparator = new Comparator<MenuElement>() {
         @Override
         public int compare(MenuElement o1, MenuElement o2) {
@@ -88,6 +163,13 @@ public class MenuElement  {
         }
     };
 
+    /**
+     * Comparator for the type of dishes. Its method compare( ) takes two MenuElements e1, e2 as parameters and returns
+     * 0 if e1 and e2  have the same price
+     * positive integer if the price of e1 is lower than the one of e2
+     * negative integer if the price of e1 is higher than the one of e2
+     *
+     */
     public static Comparator <MenuElement> priceComparator = new Comparator<MenuElement>() {
         @Override
         public int compare(MenuElement o1, MenuElement o2) {
