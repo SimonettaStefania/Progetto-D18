5647c6fb3a614eed147004a4dc40e0a56980a95e
zorzr <zorzato.riccardo@gmail.com>
2018-07-23 15:05:09 +0200

    Adjustment

--- src/servlets/SelectionServlet.java
+++ src/servlets/SelectionServlet.java
@@ -7,9 +7,10 @@ import java.io.IOException;
 
 @WebServlet(name = "SelectionServlet", urlPatterns = "/selection")
 public class SelectionServlet extends AbstractServlet {
+    private String DEFAULT_ROUTE = "/views/dishesSelection.jsp";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        forwardTo(request, response, "/views/dishesSelection.jsp");
+        forwardTo(request, response, DEFAULT_ROUTE);
     }
 
     /**
@@ -17,6 +18,6 @@ public class SelectionServlet extends AbstractServlet {
      *  Only access through post method is allowed.
      * */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        forwardTo(request, response, "/index.jsp");
+        forwardTo(request, response, INDEX_ROUTE);
     }
 }
